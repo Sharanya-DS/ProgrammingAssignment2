58f36b9041955cd1808513f83980326fe26809bf
Sharanya-DS <33083030+Sharanya-DS@users.noreply.github.com>
2017-11-17 12:31:51 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file conatins 2 functions - one that takes a matrix 
+## and prints one of the following : the matrix itsef 
+## or its inverse based on the function calls get() or getInverse() and
+## another function that checks if the value is already in the cache and
+## returns the result either from the cahce or by computing. 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  
+    inverse <- NULL
+    ## set function thaat sets the value of the matrix
+    set <- function(y){
+      x <<- y
+      inverse <<- NULL
+    }
+    
+    ## get function -> gets the value of the matrix - checking for cached values
+    get <- function() x
+    
+    setInverse <- function() inverse <<- solve(x)
+    getInverse <-  function() inverse
+    
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+    ## x <- as.list(x)
+    
+    ## Gets the inverse of the matrix and stores it in inverse.
+    inverse <- x$getInverse()
+    
+    ## Condition to check if the 'inverse' is null
+    ## if not NULL, then the value is alreay cached, so return exisitng value.
+    if(!is.null(inverse))
+    {
+      message("getting cached data")
+      return (inverse)
+    }
+    
+    ## Inverse matrix value is calculated using solve() function
+    ## and stored in cahce for further reuseage.
+    matrixData <- x$get()
+    inverse <- solve(matrixData, ...)
+    x$setInverse(inverse)
+    inverse
 }
